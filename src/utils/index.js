c2da9229bc2d253c23fa47a6d295398ccbe2d0b2
Scott <iwakoscott@gmail.com>
2018-10-31 17:33:05 -0700

    Small typo fix

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,14 @@
 import uuidv4 from "uuid/v4";
 
-export function getID() {
+function getID() {
   return uuidv4().replace(/\-/g, "");
+}
+
+export function formatIdea(title, author = "") {
+  return {
+    id: getID(),
+    title,
+    author,
+    likes: 0
+  };
 }
